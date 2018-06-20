6aaa8dc26ed72264643f33d34b9cfcb38ea88495
风 <1109695647@qq.com>
2018-06-20 22:35:34 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- wuxp_shop/shop_config/src/main/java/com/wuxp/shop/config/database/DruidConfiguration.java
+++ wuxp_shop/shop_config/src/main/java/com/wuxp/shop/config/database/DruidConfiguration.java
@@ -15,28 +15,28 @@ import org.springframework.context.annotation.Bean;
 public class DruidConfiguration {
 
 
-    @Bean
-    public ServletRegistrationBean statViewServlet(){
-        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
-        //白名单
-        servletRegistrationBean.addInitParameter("allow","127.0.01");
-        //IP黑名单（共同存在时，deny优先于allow）
-        servletRegistrationBean.addInitParameter("deny","192.168.1.100");
-        //登陆查看新的是账户密码
-        servletRegistrationBean.addInitParameter("loginUsername","druid");
-        servletRegistrationBean.addInitParameter("loginPassword","123456");
-        //是否能够重置数据
-        servletRegistrationBean.addInitParameter("resetEnable","false");
-        return servletRegistrationBean;
-    }
-
-    @Bean
-    public FilterRegistrationBean statFilter(){
-        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
-        //添加过滤规则
-        filterRegistrationBean.addUrlPatterns("/*");
-        //添加不需要忽略的格式信息
-        filterRegistrationBean.addInitParameter("exclusions","*.js.*.gif,*.png,*.css,*.ico,/druid/*");
-        return filterRegistrationBean;
-    }
+//    @Bean
+//    public ServletRegistrationBean<StatViewServlet> statViewServlet(){
+//        ServletRegistrationBean<StatViewServlet> servletRegistrationBean = new ServletRegistrationBean<>(new StatViewServlet(),"/druid/*");
+//        //白名单
+//        servletRegistrationBean.addInitParameter("allow","127.0.01");
+//        //IP黑名单（共同存在时，deny优先于allow）
+//        servletRegistrationBean.addInitParameter("deny","192.168.1.100");
+//        //登陆查看新的是账户密码
+//        servletRegistrationBean.addInitParameter("loginUsername","druid");
+//        servletRegistrationBean.addInitParameter("loginPassword","123456");
+//        //是否能够重置数据
+//        servletRegistrationBean.addInitParameter("resetEnable","false");
+//        return servletRegistrationBean;
+//    }
+//
+//    @Bean
+//    public FilterRegistrationBean<WebStatFilter> statFilter(){
+//        FilterRegistrationBean<WebStatFilter> filterRegistrationBean = new FilterRegistrationBean<>(new WebStatFilter());
+//        //添加过滤规则
+//        filterRegistrationBean.addUrlPatterns("/*");
+//        //添加不需要忽略的格式信息
+//        filterRegistrationBean.addInitParameter("exclusions","*.js.*.gif,*.png,*.css,*.ico,/druid/*");
+//        return filterRegistrationBean;
+//    }
 }
--- wuxp_shop/shop_eureka/readME.md
+++ wuxp_shop/shop_eureka/readME.md
@@ -1,4 +1,4 @@
 ####  服务注册中心
 
 #### 高可用方案（HA） 
-#### 单机版 参考 [https://blog.csdn.net/liuchuanhong1/article/details/54895070]
+#### 单机版，参加application-local.yml配置 参考 [https://blog.csdn.net/liuchuanhong1/article/details/54895070]
