2f091d80307e9cd8df11b5f19509b62c77ae874d
风 <1109695647@qq.com>
2018-06-25 21:50:26 +0800

    Signed-off-by: 风 <1109695647@qq.com>

---
+++ wuxp_shop/shop_gateway/readME.md
@@ -0,0 +1,3 @@
+####  网关
+
+#### 基础介绍 [https://juejin.im/post/5aa4eacbf265da237a4ca36f]
---
+++ wuxp_shop/shop_gateway/src/main/java/com/wuxp/shop/gateway/GatewayApplication.java
@@ -0,0 +1,34 @@
+package com.wuxp.shop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 网关
+ *
+ * @author wxup
+ * @create 2018-06-23 20:45
+ **/
+
+//表示我是一个服务,注册到服务中心上
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                //basic proxy
+                .route(r -> r.path("/baidu")
+                        .uri("http://baidu.com:80/")
+                ).build();
+    }
+}
